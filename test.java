6269f3b2c302174b5308d47f736eb5f08911f556
Kartikeychandraa <40001828+Kartikeychandraa@users.noreply.github.com>
2019-11-21 19:25:45 +0530

    UPDATED

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-class test{
+import java.awt.event.*;
+class test extends JFrame{
 public static void main(String args[])
 {
 JFrame f= new JFrame("PHARMACY MANAGEMENT");
@@ -18,9 +19,26 @@ JButton b1=new JButton("Generate Bill");
     JButton b3=new JButton("Admin Login");
     b3.setBounds(500,100,150,30);
     f.add(b3);
+
+//EXIT
+    JButton b4=new JButton("EXIT");
+    b4.setBounds(500,450,150,30);
+    f.add(b4);
+
+//action listen exit
+
+b4.addActionListener( new ActionListener()
+                       {
+                           public void actionPerformed(ActionEvent e)
+                           {f.dispose();
+
+                           }
+                        });
+	
+
+
     f.setSize(750,500);
     f.setLayout(null);
     f.setVisible(true);
-
 }
 }
