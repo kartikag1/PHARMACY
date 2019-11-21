a6a13e5bec08c67a53286e0b86292ba284018b0b
Kartikeychandraa <40001828+Kartikeychandraa@users.noreply.github.com>
2019-11-21 19:50:53 +0530

    REQUEST

--- test.java
+++ test.java
@@ -1,10 +1,41 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+//DATE LIBRARY`s
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime; 
+
 class test extends JFrame{
 public static void main(String args[])
 {
 JFrame f= new JFrame("PHARMACY MANAGEMENT");
+//DATE
+
+JTextField t1=new JTextField("");
+    t1.setBounds(5,10,150,30);
+   
+//DATE ACTION HH:mm:ss
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy ");  
+   LocalDateTime now = LocalDateTime.now();  
+   t1.setText(dtf.format(now));
+t1.setEditable(false);
+t1.setHorizontalAlignment(JTextField.CENTER);
+ f.add(t1);
+
+//HEADING
+
+JLabel l1=new JLabel("Pharmacy",JLabel.CENTER);
+              Font f1 = new Font("TimesRoman",Font.BOLD,45);
+              l1.setForeground(Color.red);
+              l1.setFont(f1);
+		f.add(l1);
+
+/*  JTextField t2=new JTextField("PHARMACY");
+    t2.setBounds(5,10,150,30);
+	Font f1 = new Font("TimesRoman",Font.BOLD,45);
+              t2.setFont(f1); */
+
+
 //pehla button
 JButton b1=new JButton("Generate Bill");
     b1.setBounds(50,100,200,30);
