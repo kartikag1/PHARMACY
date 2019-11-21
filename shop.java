8da005f589702b0129e514496cf232c675891cbf
Ayush Srivastava <33518505+ayushrko@users.noreply.github.com>
2019-11-21 22:12:22 +0530

    Update shop.java

--- shop.java
+++ shop.java
@@ -1,123 +1,296 @@
-import javax.swing.*;  
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.border.LineBorder;
-class shop extends JFrame
-{  
-    public static void main(String args[])
-    {
-              JButton b1,b2,b3;
-              JLabel l1;
-              JFrame f= new JFrame("Shop Managment"); 
-              l1=new JLabel("Pharmacy",JLabel.CENTER);
-              Font f1 = new Font("TimesRoman",Font.BOLD,45);
-              l1.setForeground(Color.red);
-              l1.setFont(f1);
-              JPanel panel=new JPanel();
-              JLabel SName = new JLabel("Shop Id:");
-              //shop no textfield
-               JTextField S1Name = new JTextField(20);
-               //
-               SName.setLabelFor(S1Name);
-              SName.setBounds(150,100,100,100);
-              S1Name.setBounds(50, 50, 100, 20);
-              JLabel SPass = new JLabel("Password :");
-              //shop password textfield
-              JTextField S1Pass = new JTextField(20);
-              //
-              SPass.setLabelFor(S1Pass);
-              SName.setFont(f1);
-              SPass.setFont(f1);
-              b1 = new JButton("Generate bills");
-              b2 = new JButton("Apparatus");
-              b3 = new JButton("Admin");
-              b3.addActionListener( new ActionListener()
-              {
-                  public void actionPerformed(ActionEvent e)
-                  {
-                      f.dispose();
-                      JFrame f1= new JFrame("Login"); 
-                      JLabel l2 = new JLabel("Login");
-                      Font f12 = new Font("TimesRoman",Font.BOLD,25);
-                      l2.setFont(f12);
-                      l2.setBounds(170,10,100,100);
-                      JLabel AName = new JLabel("Admin Id:");
-                      //admin no textfield
-                      JTextField A1Name = new JTextField(20);
-                       //
-                       AName.setLabelFor(A1Name);
-                      AName.setBounds(50,100,200,100);
-                      A1Name.setBounds(250,135, 250,35);
-                      JLabel APass = new JLabel("Password :");
-                      //shop password textfield
-                      JTextField A1Pass = new JTextField(20);
-                      //
-                      APass.setLabelFor(A1Pass);
-                      APass.setBounds(50,200,200,100);
-                      A1Pass.setBounds(250,235, 250,35);
-                      AName.setFont(f12);
-                      APass.setFont(f12);
-                      JButton b4 = new JButton("Login");
-                      b4.setBounds(210,350,120,60);
-                       b4.addActionListener( new ActionListener()
-                       {
-                           public void actionPerformed(ActionEvent e)
-                           {
-                               if(A1Name=="Admin" && A1Pass=="Admin")
-                               {
-                                // close all previous screens and open another screen containing buttons do not close this screen:-
-                                //generate bill --> link to normal bill generation screen
-                                //manage lent apparatus --> link to normal lent apparatus screen
-                                //stock management --> new screen for stock
-                                //due payments --> new screen for payments
-                                //employee shifts --> new screen for managing employees
-                                //generate balance sheet button --> new screen containing formatted balance sheet further to generate PDF and also integrate mailing system
-                               }
-                               else
-                               {
-                                // open login screen again for re-attempt
-                               }
-                           }
-                        });
-                      f1.add(l2);
-                      f1.add(AName);
-                      f1.add(A1Name);
-                      f1.add(APass);
-                      f1.add(A1Pass);
-                      f1.add(b4);
-                      f1.setSize(600,600);
-                      f1.setLayout(null);  
-                      f1.setVisible(true);
-                    }
-                });
-               
-              b1.setBounds(30,200,125,100);
-              b2.setBounds(30,350,125,100);
-              b3.setBounds(30,500,125,100);
-              
-              f.add(b1);
-              f.add(b2);
-              f.add(b3);
-              f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-              Color c = new Color(0,100,255);
-              f.getContentPane().setBackground(c);
-              
-            
-              panel.setBounds(175,40,600,650);
-              panel.setBackground(Color.green);
-              panel.add(l1);
-              panel.add(SName);
-              panel.add(S1Name);
-              panel.add(SPass);
-              panel.add(S1Pass);
-              
-              f.add(panel);
-              f.setSize(800,800);
-              f.setMinimumSize(new Dimension(800, 0));
-                  f.setMaximumSize(new Dimension(800, 800));
-              f.setLayout(null);  
-                  f.setVisible(true);  
-               //notworking
-                }
-            }
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+class pharmacyy extends JFrame {
+ public static void main(String args[]) {
+  JFrame ff = new JFrame("Pharmacy Management");
+  JLabel l = new JLabel("Pharmacy Management");
+  Font f12 = new Font("TimesRoman", Font.ITALIC, 25);
+  l.setFont(f12);
+  l.setBounds(150, 10, 450, 250);
+  JButton b = new JButton("Generate Bills");
+  b.setBounds(190, 230, 180, 100);
+  b.addActionListener(new ActionListener() {
+   public void actionPerformed(ActionEvent e) {
+    ff.dispose();
+    JFrame f = new JFrame("PHARMACY MANAGEMENT");
+    f.setTitle("BILL GENERATION");
+    //DATE
+
+    JTextField t15 = new JTextField("");
+    t15.setBounds(5, 10, 150, 30);
+
+    //DATE ACTION HH:mm:ss
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy ");
+    LocalDateTime now = LocalDateTime.now();
+    t15.setText(dtf.format(now));
+    t15.setEditable(false);
+    t15.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t15);
+
+    //CUSTOMER NAME
+    JTextField t2 = new JTextField("CUSTOEMR NAME");
+    t2.setHorizontalAlignment(JTextField.CENTER);
+    t2.setBounds(10, 50, 150, 30);
+    f.add(t2);
+
+
+    //AGE
+    JTextField t3 = new JTextField("AGE");
+    t3.setBounds(170, 50, 40, 30);
+    t3.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t3);
+
+
+    //CONTACT
+    JTextField t4 = new JTextField("CONTACT");
+    t4.setBounds(220, 50, 150, 30);
+    t4.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t4);
+
+
+    //DOCTOR NAME
+    JTextField t5 = new JTextField("DR ");
+    t5.setBounds(380, 50, 150, 30);
+    t5.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t5);
+
+
+    //PRODUCTS
+    JTextField t6 = new JTextField("PRODUCTS");
+    t6.setBounds(220, 100, 150, 30);
+    t6.setEditable(false);
+    t6.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t6);
+
+    //NAME
+    JTextField t7 = new JTextField("NAME");
+    t7.setBounds(50, 150, 150, 30);
+    t7.setEditable(false);
+    t7.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t7);
+
+    //RATE
+    JTextField t8 = new JTextField("RATE");
+    t8.setBounds(220, 150, 150, 30);
+    t8.setEditable(false);
+    t8.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t8);
+
+    //QTY
+    JTextField t9 = new JTextField("QTY");
+    t9.setBounds(390, 150, 150, 30);
+    t9.setEditable(false);
+    t9.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t9);
+
+    //ENTRY 1
+    JTextField t10 = new JTextField("");
+    t10.setBounds(50, 200, 150, 30);
+    t10.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t10);
+
+    //ENTRY 1
+    JTextField t11 = new JTextField("");
+    t11.setBounds(220, 200, 150, 30);
+    t11.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t11);
+
+    //ENTRY 1
+    JTextField t12 = new JTextField("");
+    t12.setBounds(390, 200, 150, 30);
+    t12.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t12);
+
+    //ENTRY 2
+    JTextField t13 = new JTextField("");
+    t13.setBounds(50, 250, 150, 30);
+    t13.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t13);
+
+    //ENTRY 2
+    JTextField t14 = new JTextField("");
+    t14.setBounds(220, 250, 150, 30);
+    t14.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t14);
+
+    //ENTRY 2
+    JTextField t16 = new JTextField("");
+    t16.setBounds(390, 250, 150, 30);
+    t16.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t16);
+
+
+    //ENTRY 3
+    JTextField t161 = new JTextField("");
+    t161.setBounds(50, 300, 150, 30);
+    t161.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t161);
+
+    //ENTRY 3
+    JTextField t17 = new JTextField("");
+    t17.setBounds(220, 300, 150, 30);
+    t17.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t17);
+
+    //ENTRY 3
+    JTextField t18 = new JTextField("");
+    t18.setBounds(390, 300, 150, 30);
+    t18.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t18);
+
+    //ENTRY 4
+    JTextField t19 = new JTextField("");
+    t19.setBounds(50, 350, 150, 30);
+    t19.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t19);
+
+    //ENTRY 4
+    JTextField t20 = new JTextField("");
+    t20.setBounds(220, 350, 150, 30);
+    t20.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t20);
+
+    //ENTRY 4
+    JTextField t21 = new JTextField("");
+    t21.setBounds(390, 350, 150, 30);
+    t21.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t21);
+
+    //NUMBERING 4
+
+    JTextField t22 = new JTextField("4");
+    t22.setBounds(10, 350, 30, 30);
+    t22.setEditable(false);
+    t22.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t22);
+
+    //NUMBERING 3
+
+    JTextField t23 = new JTextField("3");
+    t23.setBounds(10, 300, 30, 30);
+    t23.setEditable(false);
+    t23.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t23);
+
+
+    //NUMBERING 2 
+
+    JTextField t24 = new JTextField("2");
+    t24.setBounds(10, 250, 30, 30);
+    t24.setEditable(false);
+    t24.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t24);
+
+
+    //NUMBERING 1
+
+    JTextField t25 = new JTextField("1");
+    t25.setBounds(10, 200, 30, 30);
+    t25.setEditable(false);
+    t25.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t25);
+
+    //SPECIAL CUSTOMER
+
+    JTextField t26 = new JTextField("SPECIAL CUSTOMER");
+    t26.setBounds(10, 450, 150, 30);
+    t26.setHorizontalAlignment(JTextField.CENTER);
+    f.add(t26);
+
+
+
+
+
+
+    //EXIT
+    JButton b4 = new JButton("CONFIRM");
+    b4.setBounds(500, 450, 150, 30);
+    f.add(b4);
+
+    //action listen exit
+
+    b4.addActionListener(new ActionListener() {
+     public void actionPerformed(ActionEvent e) {
+
+     }
+    });
+
+
+
+    f.setSize(850, 700);
+    f.setLayout(null);
+    f.setVisible(true);
+   }
+  });
+  JButton b1 = new JButton("Apparatus");
+  b1.setBounds(190, 380, 180, 100);
+
+  JButton b2 = new JButton("Admin");
+
+  b2.setBounds(190, 530, 180, 100);
+  b2.addActionListener(new ActionListener() {
+   public void actionPerformed(ActionEvent e) {
+    ff.dispose();
+    JFrame f1 = new JFrame("Login");
+    JLabel l2 = new JLabel("Login");
+    Font f12 = new Font("TimesRoman", Font.ITALIC, 45);
+    l2.setFont(f12);
+    l2.setBounds(210, 10, 800, 120);
+    JLabel UName = new JLabel("Admin Id:");
+    //admin no textfield
+    JTextField U1Name = new JTextField(20);
+    //
+    UName.setLabelFor(U1Name);
+    UName.setBounds(50, 100, 200, 100);
+    U1Name.setBounds(250, 135, 250, 35);
+    JLabel UPass = new JLabel("Password :");
+    //shop password textfield
+    JPasswordField U1Pass = new JPasswordField(20);
+    //
+    String x = U1Name.getText();
+    UPass.setLabelFor(U1Pass);
+    UPass.setBounds(50, 200, 200, 100);
+    U1Pass.setBounds(250, 235, 250, 35);
+    Font f121 = new Font("TimesRoman", Font.ITALIC, 25);
+    UName.setFont(f121);
+    UPass.setFont(f121);
+    JButton b4 = new JButton("Login");
+    b4.setBounds(210, 350, 120, 60);
+    b4.addActionListener(new ActionListener() {
+     public void actionPerformed(ActionEvent e) {
+      f1.dispose();
+      JFrame f2 = new JFrame("Admin");
+      //String s = l22.setText(U1Name.getText());
+      JLabel l22 = new JLabel("");
+      l22.setBounds(70, 30, 70, 100);
+      f2.add(l22);
+      f2.setSize(600, 800);
+      f2.setLayout(null);
+      f2.setVisible(true);
+     }
+    });
+    f1.add(l2);
+    f1.add(UName);
+    f1.add(U1Name);
+    f1.add(UPass);
+    f1.add(U1Pass);
+    f1.add(b4);
+    f1.setSize(600, 600);
+    f1.setLayout(null);
+    f1.setVisible(true);
+   }
+  });
+  ff.add(b);
+  ff.add(b1);
+  ff.add(b2);
+  ff.add(l);
+  ff.setLayout(null);
+  ff.setSize(600, 800);
+  ff.setVisible(true);
+ }
+}
    
